99319f75084bd4876ef0575d570ef345dd775d00
ntademaru <ntademaru@gmail.com>
2021-11-20 00:38:58 -0800

    Login, logout and signup functionalities

--- frontend/pinteresting.jsx
+++ frontend/pinteresting.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, logout, signup } from "./actions/session_actions";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+
   ReactDOM.render(<h1>Yay you made it!</h1>, root);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
 });
