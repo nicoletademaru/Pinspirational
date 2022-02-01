f72abceeed40e2042b067e6bda582bba8c09043a
ntademaru <ntademaru@gmail.com>
2022-02-01 12:55:50 -0800

    Save button hover effect

--- frontend/components/pins/pin_item.jsx
+++ frontend/components/pins/pin_item.jsx
@@ -9,17 +9,15 @@ class PinIndexItem extends React.Component {
     const { pin, handleClick } = this.props;
     return (
       <div className='pin-index-item'>
-        <div className='pin-save'>
-          <button>Save</button>
-        </div>
-        <figure className='hover-test'>
+        <figure>
+          <button className='save-pin'>Save</button>
           <img
             key={pin.id}
             id={pin.id}
             src={pin.photoUrl}
             alt="photo"
             onClick={handleClick}
-          />
+          />        
           <figcaption>{pin.title}</figcaption>
         </figure>
       </div>
