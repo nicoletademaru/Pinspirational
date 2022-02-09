25715a1c0905b4a5266c7ae59aad2f23592dd149
ntademaru <ntademaru@gmail.com>
2022-02-09 12:47:32 -0800

    Fix refresh on pin show page

--- frontend/components/pins/pin_show.jsx
+++ frontend/components/pins/pin_show.jsx
@@ -35,37 +35,39 @@ class PinShow extends React.Component {
   }
   render() {
     if (!this.props.pin) return null;
+    if (this.props.pinboards.length === 0) return null;
+
 
     const { pin, sessionId, openModal, users, pinboards} = this.props;
 
     return (
       <div className='pin-show-page'>
-          <div className='pin-show-left'>
-            <img className='pin-show-photo' src={pin.photoUrl} alt="photo" />
+        <div className='pin-show-left'>
+          <img className='pin-show-photo' src={pin.photoUrl} alt="photo" />
+        </div>
+        <div className='pin-show-right'>
+          <div className='pin-edit'>
+          {sessionId == users[pin.author_id].id ?
+              <i className="fas fa-edit edit-pin-btn" onClick={()=> openModal('edit-pin')}></i> : "" }
           </div>
-          <div className='pin-show-right'>
-            <div className='pin-edit'>
-            {sessionId == users[pin.author_id].id ?
-                <i className="fas fa-edit edit-pin-btn" onClick={()=> openModal('edit-pin')}></i> : "" }
+          <div className='pin-show-pinning'>
+            <PinningForm
+              pinId={pin.id}
+              pinboards={pinboards}
+              handlePinning={this.handlePinning}
+              handleSave={this.handleSave}
+            />
+          </div>
+          <div className='right-show-details'>
+            <div className='pin-details'>
+              <h1>{pin.title}</h1>
+              <p>{pin.description}</p>
             </div>
-            <div className='pin-show-pinning'>
-              <PinningForm
-                pinId={pin.id}
-                pinboards={pinboards}
-                handlePinning={this.handlePinning}
-                handleSave={this.handleSave}
-              />
-            </div>
-            <div className='right-show-details'>
-              <div className='pin-details'>
-                <h1>{pin.title}</h1>
-                <p>{pin.description}</p>
-              </div>
-              <div className='pin-owner-info'>
-                <UserInfo user={users[pin.author_id]} />
-              </div>
+            <div className='pin-owner-info'>
+              <UserInfo user={users[pin.author_id]} />
             </div>
           </div>
+        </div>
       </div>
     )
   }
